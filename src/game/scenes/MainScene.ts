7cfd8a99579d31c56075e99f93ca209422e10397
hardhill <hardhill@mail.ru>
2021-10-11 19:20:35 +0800

    w3

--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -2,9 +2,13 @@ import { Scene } from 'phaser'
 
 export default class MainScene extends Scene{
 
-    
+    private graphics:Phaser.GameObjects.Graphics|null = null
+    private shapes:Array<Phaser.Geom.Circle>
+    private rect:Phaser.Geom.Rectangle
     constructor(){
         super({key:'MainScene'})
+        this.shapes = new Array<Phaser.Geom.Circle>()
+        this.rect = new Phaser.Geom.Rectangle()
     }
     preload(){
         this.load.image('back','assets/sky.png')
@@ -12,6 +16,15 @@ export default class MainScene extends Scene{
     }
     create(){
         this.add.image(400,300,'back')
+        this.graphics = this.add.graphics();
+        this.shapes = new Array(15).fill(null).map(
+            () => new Phaser.Geom.Circle(Phaser.Math.Between(0, 800), Phaser.Math.Between(0, 600), Phaser.Math.Between(25, 75))
+        );
+        let x = this.cameras.main.x
+        let y = this.cameras.main.y
+        let w = this.cameras.main.width
+        let h = this.cameras.main.height
+        this.rect = Phaser.Geom.Rectangle.Clone(new Phaser.Geom.Rectangle(x,y,w,h));
     }
 
 }
